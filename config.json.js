fa1be618a6c80a9e6a4b399c0e762cd96b2747ad
Kenney <zql254@vip.qq.com>
2017-05-22 20:32:00 +0800

    fix

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "build_date": "2017-05-22 20:28:04", 
+    "build_date": "2017-05-22 20:31:43", 
     "description": "frpc内网穿透工具", 
     "home_url": "Module_frpc.asp", 
-    "md5": "06e042c8bc37be247398022c4c4c3744", 
+    "md5": "81fe1f78d912b349b592a79e94a5ad6b", 
     "module": "frpc", 
     "name": "frpc", 
     "tar_url": "frpc/frpc.tar.gz", 
-    "title": "title of frpc", 
+    "title": "frpc", 
     "version": "0.0.2"
 }
