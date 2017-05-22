01e40194fcd2cddef58bb153f212e4a83f6d82af
Kenney <zql254@vip.qq.com>
2017-05-22 15:47:09 +0800

    add frpc

---
+++ config.json.js
@@ -0,0 +1,11 @@
+{
+    "build_date": "2017-05-22 15:46:44", 
+    "description": "frpc内网穿透工具", 
+    "home_url": "Module_frpc.asp", 
+    "md5": "d623c7a85e306cbc3f17aa862acd0e2c", 
+    "module": "frpc", 
+    "name": "frpc", 
+    "tar_url": "frpc/frpc.tar.gz", 
+    "title": "title of frpc", 
+    "version": "0.0.1"
+}
