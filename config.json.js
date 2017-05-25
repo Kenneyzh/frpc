5570429b6a2f22611fc1d807b8884fd252eec08f
Kenney <zql254@vip.qq.com>
2017-05-25 14:41:19 +0800

    fix Permission

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "build_date": "2017-05-22 20:31:43", 
+    "build_date": "2017-05-25 14:40:57", 
     "description": "frpc内网穿透工具", 
     "home_url": "Module_frpc.asp", 
-    "md5": "81fe1f78d912b349b592a79e94a5ad6b", 
+    "md5": "f5875d8ab8bcde447ff8a35e17ece2f3", 
     "module": "frpc", 
     "name": "frpc", 
     "tar_url": "frpc/frpc.tar.gz", 
     "title": "frpc", 
-    "version": "0.0.2"
+    "version": "0.0.3"
 }
--- frpc/install.sh
+++ frpc/install.sh
@@ -4,6 +4,7 @@ source $KSROOT/scripts/base.sh
 
 cp -r /tmp/frpc/* $KSROOT/
 chmod a+x $KSROOT/scripts/frpc_*
+chmod a+x $KSROOT/frpc/frpc
 
 # add icon into softerware center
 dbus set softcenter_module_frpc_install=1
